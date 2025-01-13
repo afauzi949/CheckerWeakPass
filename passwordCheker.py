35454d040485d6cc9855ef86b4d3a349acd55e5c
haikaly89 <146849185+haikaly89@users.noreply.github.com>
2025-01-13 10:21:23 +0700

    add lowr

--- passwordCheker.py
+++ passwordCheker.py
@@ -1,12 +1,13 @@
 def passwordChecker(password):
     #kriteria
     caps = any(c.isupper() for c in password)  # Apakah ada huruf kapital
+    lowr = any(c.islower() for c in password)  # Apakah ada huruf kecil
     nums = any(c.isdigit() for c in password)  # Apakah ada angka
     symb = any(c in '!@#$%^&' for c in password)  # Apakah ada simbol
     length_valid = len(password) >= 8  # Panjang password cukup
 
     # Return True jika semua kriteria terpenuhi
-    return caps and nums and symb and length_valid
+    return caps and lowr and nums and symb and length_valid
 
 # Input dari pengguna
 password = input("Masukkan password anda: ")
